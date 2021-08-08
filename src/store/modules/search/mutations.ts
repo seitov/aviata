0718a0d8af75639d5974ac586d3e003760e575e2
Nurken Seitov <seitovnurken@gmail.com>
2021-08-09 05:27:54 +0600

    [feature] add filtering for flights

--- src/store/modules/search/mutations.ts
+++ src/store/modules/search/mutations.ts
@@ -1,18 +1,25 @@
 import { SearchMutationTypes } from "@/store/modules/search/types";
-import { State } from "@/store/modules/search/state";
+import { FilterModel, State } from "@/store/modules/search/state";
 import { MutationTree } from "vuex";
 import { AirlinesModel, FlightsModel } from "@/api/search.service";
 
 export type Mutations<S = State> = {
   [SearchMutationTypes.SET_AIRLINES](state: S, payload: AirlinesModel): void;
-  [SearchMutationTypes.SET_FLIGHTS](state: S, payload: FlightsModel): void;
+  [SearchMutationTypes.SET_FLIGHTS](state: S, payload: FlightsModel[]): void;
+  [SearchMutationTypes.SET_FILTER_OPTIONS](
+    state: S,
+    payload: FilterModel
+  ): void;
 };
 
 export const searchMutations: MutationTree<State> & Mutations = {
   [SearchMutationTypes.SET_AIRLINES](state, payload: AirlinesModel) {
     state.airlines = payload;
   },
-  [SearchMutationTypes.SET_FLIGHTS](state, payload: FlightsModel) {
+  [SearchMutationTypes.SET_FLIGHTS](state, payload: FlightsModel[]) {
     state.flights = payload;
+  },
+  [SearchMutationTypes.SET_FILTER_OPTIONS](state, payload: FilterModel) {
+    state.filter = { ...payload };
   },
 };
--- src/store/modules/search/types.ts
+++ src/store/modules/search/types.ts
@@ -1,8 +1,10 @@
 export enum SearchActionTypes {
-  FETCH_JSON_DATA = "[Search] Fetch Json Data",
+  FETCH_JSON_DATA = "[Search Action] Fetch Json Data",
+  SET_FILTER_OPTIONS = "[Search Action] Set Filter Options",
 }
 
 export enum SearchMutationTypes {
-  SET_AIRLINES = "[Search] Set Airlines",
-  SET_FLIGHTS = "[Search] Set Flights",
+  SET_AIRLINES = "[Search Mutation] Set Airlines",
+  SET_FLIGHTS = "[Search Mutation] Set Flights",
+  SET_FILTER_OPTIONS = "[Search Mutation] Set Filter Options",
 }
